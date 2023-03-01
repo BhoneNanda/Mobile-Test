69c28b4c1c21264038d13d19907c55a15f37b026
BhoneNanda <bhonenanda@gmail.com>
2023-03-01 16:16:44 +0630

    March-1

---
+++ Sample-Mobile/Test Suites/Sample-Test-SUite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sample-Test-SUite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>33754bb1-1c11-464e-8649-bfeba0487c16</testSuiteGuid>
+   <testCaseLink>
+      <guid>6d45176f-2504-4af6-9e58-4f07725bd0a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample-Mobile</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
